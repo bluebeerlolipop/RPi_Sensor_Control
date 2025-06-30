83e24ad21bbd247be8815698e8f38b361f679783
기민재 <heartnebula@naver.com>
2025-06-30 18:08:51 +0900

    주석 작성

--- UDP_test/UDP_CLIENT.py
+++ UDP_test/UDP_CLIENT.py
@@ -1,7 +1,7 @@
 import socket
 
-ip = '192.168.137.1'
-port = 22
+ip = '192.168.137.1' # server의 ip를 입력해야함. client에 할당된 ip를 적으면 안됨.
+port = 22 # port번호는 수정 가능
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
