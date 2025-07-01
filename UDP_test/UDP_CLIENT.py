1c748201b15d8900b076aad83943380c6eac0b11
기민재 <heartnebula@naver.com>
2025-07-01 12:58:39 +0900

    for문 추가

--- UDP_test/UDP_CLIENT.py
+++ UDP_test/UDP_CLIENT.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 ip = '192.168.137.1' # server의 ip를 입력해야함. client에 할당된 ip를 적으면 안됨.
 port = 22 # port번호는 수정 가능
@@ -6,7 +7,9 @@ port = 22 # port번호는 수정 가능
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 message = "UDP test"
-client_socket.sendto(message.encode("UTF-8"), (ip, port))
+for i in range(10):
+    client_socket.sendto(message.encode("UTF-8"), (ip, port))
+    time.sleep(1)
 
 client_socket.close()
 print("END CONNECTION")
