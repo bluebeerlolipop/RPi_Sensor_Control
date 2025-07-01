e6022fefa2fbad66e0ae26fedf195678b3a1a260
기민재 <heartnebula@naver.com>
2025-07-01 13:41:26 +0900

    server 코드 수정 client에서 message 반복적으로 오면 message 모두 출력하게 수정 & exit이 오게 되면 socket 종료하게 구성

--- UDP_test/UDP_SERVER.py
+++ UDP_test/UDP_SERVER.py
@@ -7,8 +7,14 @@ server_socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 server_socket.bind((ip,port))
 print('WAITING FOR CLIENT...')
 data, address = server_socket.recvfrom(1024)
-print('CLIENT ip address:', address[0])
+
 while True:
     data, address = server_socket.recvfrom(1024)
+    message = data.decode("UTF-8")
     print("CLIENT ip address: ", address[0])
-    print("message from client: ",data.decode("UTF-8"))
+    print("message from client: ", message)
+    if message == "exit":
+        print("EXIT DEPLOYED. SERVER SHUTDOWN")
+        break
+
+server_socket.close()
