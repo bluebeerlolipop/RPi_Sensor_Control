b7987c72a3bf1d77da2af33e789027cc8c18613a
기민재 <heartnebula@naver.com>
2025-07-16 19:09:15 +0900

    서버 코드 수정

--- IMU_TEST/IMU_Server.py
+++ IMU_TEST/IMU_Server.py
@@ -9,39 +9,41 @@ print('WAITING FOR CLIENT...')
 data, address = server_socket.recvfrom(1024)
 print("CLIENT ip address: ", address[0])
 
-while True:
-    data, address = server_socket.recvfrom(1024)
-    message = data.decode("UTF-8")
+try:
+    while True:
+        data, address = server_socket.recvfrom(1024)
+        message = data.decode("UTF-8")
 
-    if message == "exit":
-        print("EXIT DEPLOYED. SERVER SHUTDOWN")
-        break
+        if message == "exit":
+            print("EXIT DEPLOYED. SERVER SHUTDOWN")
+            break
 
-    #message = data.decode("UTF-8")
-    message = message.strip()
-    message = message.split(',')
+        #message = data.decode("UTF-8")
+        message = message.strip()
+        message = message.split(',')
 
-    accel = [float(message[1]), float(message[2]), float(message[3])]
-    mag = [float(message[4]), float(message[5]), float(message[6])]
-    gyro = [float(message[7]), float(message[8]), float(message[9])]
-    euler = [float(message[10]), float(message[11]), float(message[12])]
-    lin_acc = [float(message[13]), float(message[14]), float(message[15])]
-    gravity = [float(message[16]), float(message[17]), float(message[18])]
-    quat = [float(message[19]), float(message[20]), float(message[21]), float(message[22])]
+        accel = [float(message[1]), float(message[2]), float(message[3])]
+        mag = [float(message[4]), float(message[5]), float(message[6])]
+        gyro = [float(message[7]), float(message[8]), float(message[9])]
+        euler = [float(message[10]), float(message[11]), float(message[12])]
+        lin_acc = [float(message[13]), float(message[14]), float(message[15])]
+        gravity = [float(message[16]), float(message[17]), float(message[18])]
+        quat = [float(message[19]), float(message[20]), float(message[21]), float(message[22])]
 
-    print(f"Accel     : {accel} m/s^2")
-    print(f"Magnet    : {mag} uT")
-    print(f"Gyro      : {gyro} deg/s")
-    print(f"Euler     : {euler} deg")
-    print(f"LinearAcc : {lin_acc} m/s^2")
-    print(f"Gravity   : {gravity} m/s^2")
-    print(f"Quaternion: {quat}")
-    print("-" * 50)
+        print(f"Accel     : {accel} m/s^2")
+        print(f"Magnet    : {mag} uT")
+        print(f"Gyro      : {gyro} deg/s")
+        print(f"Euler     : {euler} deg")
+        print(f"LinearAcc : {lin_acc} m/s^2")
+        print(f"Gravity   : {gravity} m/s^2")
+        print(f"Quaternion: {quat}")
+        print("-" * 50)
 
-    if KeyboardInterrupt:
-        print("KeyboardInterrupt: Exiting gracefully...")
-        message = "exit"
-        server_socket.sendto(message.encode("UTF-8"), (ip, port))
-        break
+except KeyboardInterrupt:
+    print("KeyboardInterrupt: Exiting gracefully...")
+    message = "exit"
+    server_socket.sendto(message.encode("UTF-8"), (ip, port))
 
-server_socket.close()
+finally:
+    server_socket.close()
+    print("Server socket closed.")
